261594ab60e0a6b812d80115400447e90ce769bd
lukasso <lukasz.koladko@gmail.com>
2021-02-28 14:04:55 +0100

    login and error pages

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  background: var(--clr-primary-10);
+  text-align: center;
+  h1 {
+    font-size: 10rem;
+  }
+  h3 {
+    color: var(--clr-grey-3);
+    margin-bottom: 1.5rem;
+  }
+`;
 
 const Error = props => {
   return (
-    <div>
-      Error page
-    </div>
+    <Wrapper>
+      <div>
+        <h1>404</h1>
+        <h3>sorry, the page you tried cannot be found</h3>
+        <Link to='/' className='btn'>
+          back home
+        </Link>
+      </div>
+    </Wrapper>
   )
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components';
+import loginImg from '../images/undraw_authentication_fsn5.svg';
+
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  .container {
+    width: 90vw;
+    max-width: 600px;
+    text-align: center;
+  }
+  img {
+    margin-bottom: 2rem;
+    max-height: 500px;
+  }
+  h1 {
+    margin-bottom: 1.5rem;
+  }
+`;
 
 const Login = props => {
   return (
-    <div>
-      Login component
-    </div>
+    <Wrapper>
+      <div className='container'>
+        <img src={loginImg} alt='github user' />
+        <h1>github user</h1>
+        <button className='btn'>
+          Log In / Sign Up
+        </button>
+      </div>
+    </Wrapper>
   )
 }
 
