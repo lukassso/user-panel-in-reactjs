417804abb953e24f86a416820b5e278720722fe5
lukasso <lukasz.koladko@gmail.com>
2021-02-28 12:52:27 +0100

    router setup

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { Dashboard, Error, Login } from "./pages"
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path="/" exact={true}>
+            <Dashboard />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="*">
+            <Error />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Error.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Error = props => {
+  return (
+    <div>
+      Error page
+    </div>
+  )
+}
+
+Error.propTypes = {
+
+}
+
+export default Error
