1a9729a97b01cb71834d8d2af83c8e84159dc2e0
lukasso <lukasz.koladko@gmail.com>
2021-03-14 20:53:27 +0100

    Doughnut; most popular leanguages

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -2,6 +2,7 @@ import React from "react"
 import { GithubContext } from "../context/context"
 import styled from "styled-components"
 import Pie from "./Charts/Pie"
+import Doughnut from "./Charts/Doughnut"
 
 const Wrapper = styled.div`
   display: grid;
@@ -48,32 +49,48 @@ const Repos = () => {
 
   let languages = repos.reduce((total, item) => {
     // console.log(item);
-    const { language } = item
+    const { language, stargazers_count } = item
     // console.log(language)
     if (!language) return total
     if (!total[language]) {
-      total[language] = { label: language, value: 1 }
+      total[language] = { 
+        label: language, 
+        value: 1, 
+        stars: stargazers_count 
+        };
     } else {
       total[language] = {
         ...total[language],
         value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
       }
     }
     return total
   }, {})
-  // console.log(languages)
+  console.log(languages)
   
-  languages = Object.values(languages)
+  const mostUsed = Object.values(languages)
     .sort((a, b) => {
       return b.value - a.value
     })
     .slice(0, 4)
   console.log(languages)
 
+const mostPopular = Object.values(languages)
+.sort((a, b) => {
+  return b.stars - a.stars
+})
+.map(item => {
+  return({...item, value:item.stars})
+});
+console.log(mostPopular)
+
+
   return (
     <section className="section">
       <Wrapper className="section-center">
-        <Pie data={languages} />
+        <Pie data={mostUsed} />
+        <Doughnut data={mostPopular}/>
       </Wrapper>
     </section>
   )
